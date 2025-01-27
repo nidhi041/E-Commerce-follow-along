cb7e9d5d2b555dbc9bfc54f6f07533ff657dda70
nidhi namdev <nidhi.namdev@kalvium.community>
2025-01-27 11:18:39 +0530

    added

--- backend/controller/user.js
+++ backend/controller/user.js
@@ -4,11 +4,15 @@ const User = require('../model/user');
 const router = express.Router();
 const {upload}=require('../multer');
 const ErrorHandler = require('../utils/ErrorHandler');
+const catchAsyncErrors = require("../middleware/catchAsyncError");
+const jwt = require("jsonwebtoken");
+const sendMail = require("../utils/sendMail");
+
 
 //create user
 router.post(
     "/create-user",
-    upload.single("file"),
+    upload.single("file"), 
     async (req, res, next) => {
         const { name, email, password } = req.body;
         const userEmail = await User.findOne({ email });
